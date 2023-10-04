391c9a91e93a1dcdcd28c3c46f02e690bdbb637c
Mohamed Sherif <114280645+Eng-frontend@users.noreply.github.com>
2023-10-04 10:37:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+let room_link = document.getElementById("room_link");
+room_link.onclick=function(){
+window.scrollTo({
+  top:1050,
+  behavior:"smooth"
+});
+};
+let about_link = document.getElementById("about_link");
+about_link.onclick=function(){
+    window.scrollTo({
+      top:4540,
+      behavior:"smooth"
+});
+};
+    let contact_link = document.getElementById("contact_link");
+    contact_link.onclick=function(){
+        window.scrollTo({
+          top:8900,
+          behavior:"smooth"
+});
+};
+let book_link = document.getElementById("book_link");
+book_link.onclick=function(){
+    window.scrollTo({
+      top:398,
+      behavior:"smooth"
+});
+};
+/*-------------------------------------*/
